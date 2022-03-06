91a6824becaa0260c9a158ae5c86830bb24c02e5
ming9mon <koo50215@gmail.com>
2022-03-07 02:00:35 +0900

    .test

--- src/main/java/kr/ming9/boot/test/domain/posts/PostsRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.ming9.boot.test.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {   //entity 클래스, pk 타입
-}
